8db1636d57ea776cab6614f37218ca8694cc65eb
James Goff <james.goff1030@gmail.com>
2022-04-29 11:33:24 -0700

    more completed

--- about.js
+++ about.js
@@ -1,9 +1,9 @@
-console.log("hello world");
+alert("Form Submitted");
 
 
 
 function showAlert() {
-    count = 0;
+    count = FormSubmitted;
     alert(count);
     counterText.textContent = count;
     console.log(counterText)
@@ -12,5 +12,8 @@ function showAlert() {
 
 
 let form = document.querySelector('form#contact');
+let catPic = document.querySelector('https://p1-tt.byteimg.com/origin/tos-cn-i-qvj2lq49k0/cac66f77ebb14cf38c2d9afbd95003cf.jpg');
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit',showAlert);
+
+pic.addEventListener('mouseover')
