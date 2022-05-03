73119414ac290235163176c01ce5cff499da198d
James Goff <james.goff1030@gmail.com>
2022-05-03 14:50:59 -0700

    Redone

--- about.js
+++ about.js
@@ -17,3 +17,26 @@ let catPic = document.querySelector('https://p1-tt.byteimg.com/origin/tos-cn-i-q
 form.addEventListener('submit',showAlert);
 
 pic.addEventListener('mouseover')
+
+
+
+
+
+
+
+
+//step 1 grab the html element
+const image = document.querySelector("img");
+
+
+
+// step 2 write callback function:
+
+function handleMouseOver() {
+	alert("Hey, you are beautiful")
+}
+
+
+//step 3 combing using addEventListener
+form.addEventListener('submit', handlesubmit) 
+image.addEventListener("mouseover", handleMouseOver)
--- profile.js
+++ profile.js
@@ -29,3 +29,64 @@ function myFaveColor() {
 colorBtn.addEventListener("click", myFaveColor);
 placeBtn.addEventListener("click", myFavePlace);
 ritualBtn.addEventListener("click", myFaveRitual);
+
+
+
+
+
+//step 1 grab html Elements
+const profileBtns = document.querySelectorAll(".profile-buttons") 
+
+
+//step 2 write functions
+function color() {
+    alert("my favorite color is blue")
+}
+
+function place() {
+    alert("my favorite place is home")
+}
+
+function ritual() {
+    alert("going to the gym")
+}
+
+for(let i = 0; i < profileBtns.length; i++) {
+    if(profileBtns[i].id === "color") {
+        profileBtns[i].addEventListener("click", color);
+    } else if(profileBtns[i].id === "place"); {
+        profileBtns[i].addEventListener("click", place);
+    } else if (profileBtns[i].id === "ritual") {
+        profileBtns[i].addEventListener("click", ritual);
+    }
+}
+
+
+//for-each with a switch statement
+profileBtns.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        let favorite = "";
+        switch(event.target.id) {
+            case "color":
+                favorite = "blue";
+                break;
+             case "place":
+                 favorite = "my home";
+                 break;
+             case "ritual":
+                 favorite = "going to the gym";
+                 break;       
+        }
+
+        alert (`My favorite ${event.target.id} is ${favorite}`);
+    })
+}) 
+
+
+
+
+
+//step 3: combine using addEventListener
+colorBtn.addEventListener("click", color);
+placeBtn.addEventListener("click", place);
+ritualBtn.addEventListener("click", ritual);
